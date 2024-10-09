23a4c52c3cee06486d996e3d549ccc8b1b97e9d1
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-10-09 07:52:02 -0600

    Terminando GenericClassForm

---
+++ GenericClassForm/Addition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClassForm
+{
+    // Clase para realizar suma, permitiendo conversiones
+    internal class Addition<T> : IOperation<T> where T : struct, IConvertible
+    {
+        public T Operate(T a, T b)
+        {
+            // Convertir a double para realizar la operación suma
+            double sum = Convert.ToDouble(a) + Convert.ToDouble(b);
+
+            // Convertir el resultado d enuevo al tipo T
+            return (T)Convert.ChangeType(sum, typeof(T));
+        }
+    }
+}
---
+++ GenericClassForm/MathOperationForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenericClassForm
+{
+    public partial class MathOperationForm : Form
+    {
+        private MathOperationHandler _handler;
+        public MathOperationForm(MathOperationHandler handler)
+        {
+            InitializeComponent();
+            _handler = handler;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Obtener el tipo de operación
+                Type operationType = _handler.GetOperationType();
+
+                // Declarar variables dinámicas
+                dynamic value1 = null;
+                dynamic value2 = null;
+
+                // Convertir entradas a tipo específico
+                value1 = Convert.ChangeType(txtInput1.Text, operationType);
+                value2 = Convert.ChangeType(txtInput2.Text, operationType);
+
+                // Realizar la operación
+                string result = _handler.PerformOperation(value1, value2);
+
+                // Mostrar el resultado
+                lblResult.Text = "Result: " + result;
+            }
+            catch
+            {
+                lblResult.Text = "Invalid input or type mismatch!";
+            }
+
+        }
+    }
+}
