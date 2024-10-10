9925faa4e20585a5e3bfd9728cce62322c5239f6
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-10-10 10:40:44 -0600

    GenericComparisonApp

---
+++ GenericComparisonApp/Form1.cs
@@ -0,0 +1,57 @@
+namespace GenericComparisonApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lblResult.Text = "";
+        }
+
+        private void btnCompare_Click(object sender, EventArgs e)
+        {
+            CompareValues<decimal>();
+        }
+
+        private void CompareValues<T>() where T : IComparable<T>
+        {
+            try
+            {
+                T value1 = (T)Convert.ChangeType(txtInput1.Text, typeof(T));
+                T value2 = (T)Convert.ChangeType(txtInput2.Text, typeof(T));
+
+                // Comparar los valores usando IComparable<T>
+                int comparisonResult = value1.CompareTo(value2);
+
+                if (comparisonResult > 0)
+                {
+                    lblResult.Text = "El primer valor es mayor que el segundo.";
+                }
+                else if (comparisonResult < 0)
+                {
+                    lblResult.Text = "El primer valor es menor que el segundo.";
+                }
+                else
+                {
+                    lblResult.Text = "Ambos valores son iguales.";
+                }
+                lblResult.ForeColor = Color.Green;
+            }
+            catch (InvalidCastException)
+            {
+                lblResult.Text = "Error: No se pueden convertir los valores al tipo especificado.";
+                lblResult.ForeColor = Color.Red;
+            }
+            catch (FormatException)
+            {
+                lblResult.Text = "Error: El formato de uno ambos valores es incorrecto.";
+                lblResult.ForeColor = Color.Red;
+            }
+            catch (Exception ex)
+            {
+                lblResult.Text = $"Error inesperado: {ex.Message}";
+                lblResult.ForeColor = Color.Red;
+            }
+        }
+    }
+}
