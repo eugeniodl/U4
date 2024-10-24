f29dfafda37aeeac20a8171119e5712d21c8ec5b
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-10-24 11:57:41 -0600

    ProgramaAcademicoApp

---
+++ ProgramaAcademicoApp/ProgramaAcademico.cs
@@ -0,0 +1,12 @@
+﻿public class ProgramaAcademico
+{
+    public string NombreProgramaAcademico { get; set; }
+    public List<Estudiante> Estudiantes { get; set;}
+
+    public ProgramaAcademico(string nombreProgramaAcademico)
+    {
+        NombreProgramaAcademico = nombreProgramaAcademico;
+        Estudiantes = new List<Estudiante>();
+    }
+}
+
